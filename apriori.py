48bba85a0426ea03aa55b496fbad2bc149a65cc4
yav-d <yavor.d0@gmail.com>
2022-01-25 10:19:34 -0600

    comment

--- apriori.py
+++ apriori.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from dataclasses import make_dataclass
 
+
+# Sample data set
 df = pd.DataFrame(np.array([
     'b,a,c',
     'a,c',
@@ -9,12 +11,20 @@ df = pd.DataFrame(np.array([
     'c'
 ]), columns=['items'])
 
+class Apriori:
+    pass
 
 def find_support(
     pd_series: pd.Series,
     min_support: int = 1,
     index_type: str = 'str'
-):
+) -> pd.DataFrame:
+    """
+    pd_series: iterate through sets and find the support for each item in the item set
+    min_support: minimum support threshold for final item sets
+    index_type: based on input data and if the user would like it to be encoded as int for ex
+    output (L): pd.DataFrame with support counts that pass threshold and displays set size
+    """
     split = pd_series.str.split(',', expand=True)
     support = split.apply(pd.value_counts).sum(axis=1).where(lambda value: value > min_support).dropna()
     L = pd.DataFrame(
@@ -24,7 +34,3 @@ def find_support(
         'set_size': 1
     })
     return(L)
-
-x = find_support(df['items'])
-df['set_size'] = df['items'].str.count(',') + 1
-df['items'] = df['items'].apply(lambda row: set(map(str, row.split(","))))
