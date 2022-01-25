9ecb659c0bee3090f8120d1948671f32ff3b48a7
yav-d <yavor.d0@gmail.com>
2022-01-25 11:40:30 -0600

    subset support wip

--- apriori.py
+++ apriori.py
@@ -1,5 +1,8 @@
+import imp
 import pandas as pd
 import numpy as np
+from itertools import combinations
+from operator import itemgetter
 from dataclasses import make_dataclass
 
 
@@ -20,10 +23,8 @@ def find_support(
     index_type: str = 'str'
 ) -> pd.DataFrame:
     """
-    pd_series: iterate through sets and find the support for each item in the item set
-    min_support: minimum support threshold for final item sets
-    index_type: based on input data and if the user would like it to be encoded as int for ex
-    output (L): pd.DataFrame with support counts that pass threshold and displays set size
+    calculate support for each item in pd_series
+    output items that pass the min_support threshold
     """
     split = pd_series.str.split(',', expand=True)
     support = split.apply(pd.value_counts).sum(axis=1).where(lambda value: value > min_support).dropna()
@@ -35,14 +36,29 @@ def find_support(
     })
     return(L)
 
-def do_apriori(
+def find_subset_support(
     data: pd.DataFrame = df,
     min_support: int = 1,
 ):
-    L = find_support(data['items'])
+    """
+    calculate the support for all possible subsets
+    """
+    L = find_support(data['items'], min_support=min_support)
+    data['set_size'] = data['items'].str.count(",") + 1
     data['items'] = data['items'].apply(lambda row: set(map(str, row.split(','))))
     L_set = set(L['items'])
 
-    return(L_set)
-
-print(do_apriori)
+    for length in range(2, len(L_set)+1):
+        data = data[data['set_size']>= length]
+        d = data['items'] \
+            .apply(lambda st: pd.Series(s if set(s).issubset(st) else None for s in combinations(L_set, length))) \
+            .apply(lambda col: [col.dropna().unique()[0], col.count()] if col.count() >= min_support else None).dropna()
+        if d.empty:
+            break
+        print(d.values[0])
+        # L = L.append(pd.DataFrame({
+        #     'items': list(map(itemgetter(0), d.values)),
+        #     'support': list(map(itemgetter(1), d.values)),
+        #     'set_size': length
+        #      }), ignore_index=True)
+    
