4d6a86c1abfb46e1c14e2da52cb2789c84a7882b
yav-d <yavor.d0@gmail.com>
2022-01-28 10:37:44 -0600

    confidence calculation done

--- apriori.py
+++ apriori.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from itertools import combinations
 from operator import itemgetter
-from dataclasses import make_dataclass
+
 
 # import sample data
 # combinations of individual items coded as str -> list of ints
@@ -13,7 +13,7 @@ class Apriori:
 
 def find_support(
     pd_series: pd.Series,
-    min_support: int = 1,
+    min_support: int = len(df)*0.25,
     index_type: str = 'int'
 ) -> pd.DataFrame:
     """
@@ -25,7 +25,7 @@ def find_support(
     L = pd.DataFrame(
     {
         'items': support.index.astype(index_type),
-        'support_count': support.values,
+        'support': support.values,
         'set_size': 1
     })
     return(L)
@@ -39,6 +39,7 @@ def find_subset_support(
     """
     L = find_support(data['items'], min_support=min_support)
     data['set_size'] = data['items'].str.count(",") + 1
+    # change map based on data type, ideally should be int
     data['items'] = data['items'].apply(lambda row: set(map(int, row.split(','))))
     L_set = set(L['items'])
 
@@ -49,9 +50,26 @@ def find_subset_support(
             .apply(lambda col: [col.dropna().unique()[0], col.count()] if col.count() >= min_support else None).dropna()
         if d.empty:
             break
+
         L = L.append(pd.DataFrame({
             'items': list(map(itemgetter(0), d.values)),
-            'support_count': list(map(itemgetter(1), d.values)),
+            'support': list(map(itemgetter(1), d.values)),
             'set_size': length
              }), ignore_index=True)
+
         return(L)
+
+def find_confidence(
+    data: pd.DataFrame = find_subset_support(),
+    min_confidence: float = 0.5
+) -> pd.DataFrame:
+    """
+    calculate confidence using support of all sets passing min_support threshold
+    """
+    s = data[data['set_size'] < 2]
+    m = data[data['set_size'] >= 2]
+
+    m['confidence'] = m['support'] / s.loc[s['items'] == m['items'].values[0][0], ['support']].values[0]
+    m = m[m['confidence'] >= min_confidence]
+
+    return(m)
